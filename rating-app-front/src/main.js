d53eec3a2c05c54867ded3d24f142c4745772376
Samuel Kitazume <samuel.kitazume@gmail.com>
2019-05-30 21:10:59 -0300

    Add vue-axios-plugin

--- rating-app-front/src/main.js
+++ rating-app-front/src/main.js
@@ -1,11 +1,26 @@
 import Vue from 'vue';
-import Element from 'element-ui';
+
+import VueAxiosPlugin from 'vue-axios-plugin';
 import GAuth from 'vue-google-oauth2';
+import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+Vue.use(VueAxiosPlugin, {
+  reqHandleFunc: function reqHandleFunc(config) {
+    const imaproLocalStorage = JSON.parse(localStorage.getItem('imapro-app'));
+    const Authorization = imaproLocalStorage ? imaproLocalStorage.authorization : {};
+    return Object.assign({}, config, {
+      baseURL: 'http://localhost:3001/',
+      headers: {
+        Authorization: `Bearer ${Authorization}`,
+      },
+    });
+  },
+});
 Vue.use(Element);
 Vue.use(GAuth, { clientId: '982245226486-383gkf6rcnnfkmglkaltsf1j8nsm9gr7.apps.googleusercontent.com' });
 Vue.config.productionTip = false;
